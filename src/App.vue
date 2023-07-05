b6cd6d397d5c0f1407e02fd5af9545d6bd3617ad
bimbims125 <pramvendread@gmail.com>
2023-07-05 14:30:23 +0700

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <Header/>
   <router-view/>
+  <Footer/>
 </template>
 <script>
+import Footer from './components/Footer.vue';
 import Header from './components/Header.vue';
+
 export default{
   components:{
-    Header: Header
+    Header: Header,
+    Footer:Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="py-2 text-muted bg-dark">
+
+    <p class="text-center text-muted">Created by <a href="https://github.com/bimbims125"
+        class="text-decoration-none">Pramdhanni Dwi Putra Bintang</a></p>
+    <p class="text-center text-muted">API by <a href="https://equran.id/apidev"
+        class="text-decoration-none">equran.id</a></p>
+    <p class="text-center text-muted">Template by <a href="https://github.com/azkazikna"
+        class="text-decoration-none">Azkazikna Ageung Laksana</a></p>
+
+  </footer>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
